4c6960ab79403f2210d6f0ca44118b506962b700
Mateusz Derks <mateusz.derks@schibsted.pl>
2015-02-17 22:35:05 +0100

    display popup on update ready

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,4 +4,43 @@
     var app = document.querySelector('#app');
     app.appName = 'Terra Mystica lobby';
     app.playersNum = 3;
+    app.onDownloadNewVersion = function () {
+        setTimeout(function () {
+            window.location.reload();
+        }, 400);
+    };
+
+    function waitForTemplateBound() {
+        var templateBound = Q.defer();
+
+        app.addEventListener('template-bound', function() {
+            templateBound.resolve();
+        });
+
+        return templateBound.promise;
+    }
+
+    function waitForUpdateReady() {
+        var updateReady = Q.defer();
+
+        if (window.applicationCache) {
+            window.applicationCache.addEventListener('updateready', function () {
+                if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
+                    updateReady.resolve();
+                } else {
+                    updateReady.reject();
+                }
+            }, false);
+        } else {
+            updateReady.reject();
+        }
+
+        return updateReady.promise;
+    }
+
+    Q.all([waitForTemplateBound(), waitForUpdateReady()])
+        .then(function () {
+            document.querySelector('#updatePopup').toggle();
+        });
+
 })(wrap(document));
